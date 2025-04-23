5b8707c04fd54963a4c102ba3a28be6b89a26117
RianHadi096 <rian.hadi@widyatama.ac.id>
2025-04-23 11:05:14 +0700

    Update Form Login 2

--- app/Views/login.php
+++ app/Views/login.php
@@ -15,12 +15,13 @@
         <div class="col" style="background: radial-gradient(50% 123.47% at 50% 50%, #ffc700 0%, #720059 100%), linear-gradient(121.28deg, #669600 0%, #ff4d00 100%), linear-gradient(360deg, #ff6b00 0%, #8fff00 100%), radial-gradient(100% 164.72% at 100% 100%, #6100ff 0%, #00ff57 100%), radial-gradient(100% 148.07% at 0% 0%, #fff500 0%, #6a845a 100%);background-blend-mode: screen, color-dodge, overlay, difference, normal;padding: 50px;margin-bottom: 2px;padding-bottom: 160px;">
             <div class="login-card"><img class="profile-img-card" src="<?= base_url('img/avatar_2x.png');?>">
                 <p class="profile-name-card"> </p>
-                <p style="text-align: center;">Login untuk akses BEEKL</p>
+                <p style="text-align: center;">Login untuk akses Beekl</p>
                 <form class="form-signin" style="padding-bottom: 52px;margin-bottom: -34px;" action="/login/auth" method="post">
                     <span class="reauth-email"> </span>
                     <input class="form-control" type="email" name="login_email" id="InputForEmail" required="" placeholder="Email address" autofocus="" value="<?= set_value('login_email')?>">
                     <input class="form-control" type="password" name="login_password" id="InputForPassword" required="" placeholder="Password">
                     <button class="btn btn-primary btn-lg d-block btn-signin w-100" type="submit" style="padding-bottom: 0px;margin-bottom: 0px;">Masuk</button>
+                
                 </div>
         </div>
     </div>
